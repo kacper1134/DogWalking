eb09d13b8fb603a6f5e772f134c9f526048d47d8
Kopyn <mackopin@gmail.com>
2023-04-09 00:42:17 +0200

    Added DataBaseContext

---
+++ backend/DogWalkingAPI/DogWalkingAPI/Model/DataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DogWalkingAPI.Model
+{
+    public class DataBaseContext : DbContext
+    {
+        protected DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+
+        public DbSet<Dog> Dogs { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<Availability> Availabilities { get; set; }
+    }
+}
