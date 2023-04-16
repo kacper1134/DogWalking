6ff433bbdfa2ee20d2297cdc4c1e1616cd526080
Kopyn <mackopin@gmail.com>
2023-04-17 01:07:17 +0200

    Added connection string setup

--- backend/DogWalkingAPI/DogWalkingAPI/Program.cs
+++ backend/DogWalkingAPI/DogWalkingAPI/Program.cs
@@ -1,6 +1,10 @@
+using DogWalkingAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
